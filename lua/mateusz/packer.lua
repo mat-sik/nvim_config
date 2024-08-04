c99ca3f778ca265626c3b9bc4a57d1a81fbddddf
mateusz <siiikor.sikorski@gmail.com>
2024-08-04 22:28:48 +0200

    Added new required dependency for nivm dap

--- lua/mateusz/packer.lua
+++ lua/mateusz/packer.lua
@@ -22,7 +22,10 @@ return require('packer').startup(function(use)
     use('mfussenegger/nvim-dap')
     use {
         "rcarriga/nvim-dap-ui",
-        requires = { "mfussenegger/nvim-dap" }
+        requires = { 
+            { "mfussenegger/nvim-dap" }, 
+            { "nvim-neotest/nvim-nio" } 
+        }
     }
     use('mfussenegger/nvim-dap-python')
     use {
